9ad8c39d592cb417c31fcd164409df10bdaa18b4
Vinay0264 <vinayv0264@gmail.com>
2025-02-16 12:48:19 +0530

    added image sliding

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function toggleSidebar() {
         sidebar.style.display = "flex"; // Show sidebar
     }
 }
+// sliding functionalty
+var swiper = new Swiper(".mySwiper", {
+    loop: true,               // Enables infinite scrolling
+    autoplay: {
+        delay: 3000,          // 3 seconds per slide
+        disableOnInteraction: false, // Keeps autoplay running after user interaction
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
